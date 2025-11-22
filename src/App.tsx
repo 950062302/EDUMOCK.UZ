9328fd5cae41c5ebbfc7349668c3b25d3752950b
[dyad] <abdulmalikeraliyev08@gmail.com>
2025-11-22 16:16:11 +0500

    [dyad] Moved EduAi Assistant button to top right with top-down animation - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -100,10 +100,10 @@ const App = () => {
             
             {/* Floating EduAi Assistant Button */}
             <motion.div
-              initial={{ opacity: 0, y: 50 }}
+              initial={{ opacity: 0, y: -50 }} {/* Yuqoridan pastga tushish uchun y: -50 */}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 1 }} // Sahifa yuklangandan 1 soniya o'tib paydo bo'ladi
-              className="fixed bottom-4 right-4 z-[9999]"
+              transition={{ duration: 0.5, delay: 1 }}
+              className="fixed top-4 right-4 z-[9999]" {/* Yuqori o'ng burchakka joylashtirish */}
             >
               <Button
                 variant="default"
