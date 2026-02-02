276e215dcf7fa82e4477b268f4ea4a4bc763bb22
[dyad] <abdulmalikeraliyev08@gmail.com>
2026-02-02 15:41:53 +0500

    [dyad] Orqa fondagi harflarni yulduzli fon bilan almashtirish - wrote 3 file(s), deleted 1 file(s)

---
+++ src/components/StarBackground.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+
+const StarBackground: React.FC = () => {
+  return (
+    <div className="container">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  );
+};
+
+export default StarBackground;
